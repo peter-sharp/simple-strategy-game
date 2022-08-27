e4722cc6b66db1439640ab879f9d539b236a8fcd
Peter Sharp <hello@petersharp.dev>
2022-08-26 21:13:37 -0600

    Adds comment

--- game/getByProp.js
+++ game/getByProp.js
@@ -1,3 +1,10 @@
+/**
+ * Return item in given array with given prop matching given value
+ * @param {String} prop 
+ * @param {Array} xs 
+ * @param {*} value 
+ * @returns {Object}
+ */
 export default function getByProp(prop, xs, value) {
     return xs.find(x => x[prop] == value)
 }
